bfd3d75b60c49f148e17822d7e90fc95544c2d6a
Maksym Hrytsiuk <maxeemja.inc@gmail.com>
2026-08-28 10:59:29 +0200

    feat: extend EventCard, add navbar + light rays lib

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import ExploreBtn from '@/components/ExploreBtn';
+import EventCard from '@/components/EventCard';
+import { events } from '@/lib/constants';
 
 const RootPage = () => {
-  return <div>RootPage</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for every Dev <br /> Event you can&#39;t miss!
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, meetups and conferences. All in One Place
+      </p>
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured events</h3>
+
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 export default RootPage;
---
+++ components/EventCard.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Event } from '@/lib/constants';
+
+const EventCard = ({ title, image, slug, location, date, time }: Event) => {
+  return (
+    <Link href={`/events/${slug}`} id="event-card">
+      <Image
+        src={image}
+        alt={title}
+        width={410}
+        height={300}
+        className="poster"
+      />
+
+      <div className="flex gap-2">
+        <Image src="/icons/pin.svg" alt="location pin" width={14} height={14} />
+        <p>{location}</p>
+      </div>
+
+      <p className="title">{title}</p>
+
+      <div className="datetime">
+        <div>
+          <Image
+            src="/icons/calendar.svg"
+            alt="location calendar"
+            width={14}
+            height={14}
+          />
+          <p>{date}</p>
+        </div>
+        <div>
+          <Image
+            src="/icons/clock.svg"
+            alt="location clock"
+            width={14}
+            height={14}
+          />
+          <p>{time}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default EventCard;
